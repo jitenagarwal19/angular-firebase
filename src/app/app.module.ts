40ccc60333f061c847818dfa892d7d4dce5fb94d
jitenagarwal <jitenagarwal19@gmail.com>
2017-03-25 11:24:39 +0530

    first-setup-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AngularFireModule} from 'angularfire2';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from './components/app-components/app.component';
+
+export const firebaseConfig =  {
+    apiKey: "AIzaSyD2spTcblilPgr5ybQBtg6NvCSeBHc6DME",
+    authDomain: "angular-2-ae74c.firebaseapp.com",
+    databaseURL: "https://angular-2-ae74c.firebaseio.com",
+    storageBucket: "angular-2-ae74c.appspot.com",
+    messagingSenderId: "991992043678"
+  };
+
+
 
 @NgModule({
   declarations: [
@@ -12,6 +24,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     HttpModule
   ],
   providers: [],
