cc12b551c0dfe639bd0cebe16cc23fbbe19b5a61
jitenagarwal <jitenagarwal19@gmail.com>
2017-03-28 10:53:18 +0530

    upload Image coded migrated to service

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,21 +1,22 @@
-import {Image} from '../../interfaces/Image';
-import {Component, Input, Inject, Output, EventEmitter} from '@angular/core';
-import {FirebaseListObservable,FirebaseObjectObservable, AngularFire, FirebaseApp} from 'angularfire2';
-import {UtilityService} from '../../services/utilities.service';
-import {Observable} from 'rxjs';
+import { Image } from '../../interfaces/Image';
+import { Component, Input, Inject, Output, EventEmitter } from '@angular/core';
+import { FirebaseListObservable, FirebaseObjectObservable, AngularFire, FirebaseApp } from 'angularfire2';
+import { UtilityService } from '../../services/utilities.service';
+import { ImageManagementService } from '../../services/image-management.service';
+
 
 @Component({
-    selector:'image-upload',
-    templateUrl:'./image-upload.html'
+    selector: 'image-upload',
+    templateUrl: './image-upload.html'
 })
 export class ImageUpload {
     @Input() folder: string;
     @Output() imageUploaded = new EventEmitter();
-    isUploading:boolean = false;
-    firebaseApp:any;
-    fileUploadStatus:string = "Please choose a file";
+    isUploading: boolean = false;
+    firebaseApp: any;
+    fileUploadStatus: string = "Please choose a file";
 
-    constructor(public af:AngularFire, @Inject(FirebaseApp) firebaseApp: any, private utilityService: UtilityService) {
+    constructor(public af: AngularFire, @Inject(FirebaseApp) firebaseApp: any, private utilityService: UtilityService, private imageManagementService: ImageManagementService) {
         this.firebaseApp = firebaseApp;
     }
 
@@ -23,32 +24,49 @@ export class ImageUpload {
 
     }
     ngOnChanges() {
-        
+
     }
+    // uploadImage() {
+    //     let storage = this.firebaseApp.storage().ref();
+    //     let generatedKey: string = this.utilityService.generateRandomString();
+    //     let that = this;
+    //     this.isUploading = true;
+    //     for (let selectedFile of [(<HTMLInputElement>document.getElementById('file-upload')).files[0]]) {
+    //         if (!this.utilityService.isFileNameImage(selectedFile.name)) {
+    //             this.fileUploadStatus = "Select appropriate Format of Image File";
+    //             this.isUploading = false;
+    //             return;
+    //         }
+    //         console.log("Name of selectedFile " + selectedFile.name);
+    //         let path = `/images/${that.folder}/${selectedFile.name}`;
+    //         let iRef = storage.child(path);
+    //         iRef.put(selectedFile).then((snapshot) => {
+    //             console.log('file uploading successful');
+    //             let imageObject: any = { path: path, fileName: selectedFile.name, key: generatedKey };
+    //             that.af.database.object(`/images/${generatedKey}`).set(imageObject);
+    //             this.imageUploadComplete(imageObject);
+    //         })
+    //     }
+    // }
     uploadImage() {
-        let storage = this.firebaseApp.storage().ref();
-        let generatedKey:string = this.utilityService.generateRandomString();
-        let that = this;
-        this.isUploading = true;
+        var that = this;
         for (let selectedFile of [(<HTMLInputElement>document.getElementById('file-upload')).files[0]]) {
             if (!this.utilityService.isFileNameImage(selectedFile.name)) {
                 this.fileUploadStatus = "Select appropriate Format of Image File";
-                this.isUploading =  false;
+                this.isUploading = false;
                 return;
             }
-            console.log("Name of selectedFile " + selectedFile.name);
-            let path = `/images/${that.folder}/${selectedFile.name}`;
-            let iRef = storage.child(path);
-            iRef.put(selectedFile).then((snapshot)=> {
-                console.log('file uploading successful');
-                let imageObject:any = {path:path, fileName:selectedFile.name};
-                that.af.database.object(`/images/${generatedKey}`).set(imageObject);
-                this.imageUploadComplete(imageObject);
-            })
+            this.imageManagementService.uploadImage(selectedFile, this.folder, function (imageObj) {
+                that.af.database.object(`/images/${imageObj.key}`).set(imageObj);
+                that.imageUploadComplete(imageObj);
+            });
+
+
         }
     }
-    private imageUploadComplete(imageObject:any) {
+    private imageUploadComplete(imageObject: any) {
         this.isUploading = false;
+        this.fileUploadStatus = "Image Upload successful";
         this.imageUploaded.emit(imageObject);
     }
 
--- src/app/services/image-management.service.ts
+++ src/app/services/image-management.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, Inject } from '@angular/core';
 import {AngularFire, FirebaseObjectObservable, FirebaseListObservable, FirebaseApp} from 'angularfire2';
+import {UtilityService} from './utilities.service';
 @Injectable()
 export class ImageManagementService {
     firebaseApp: any;
-    constructor(private af: AngularFire, @Inject(FirebaseApp) firebaseApp:any) {
+    constructor(private af: AngularFire, @Inject(FirebaseApp) firebaseApp:any, private utilityService:UtilityService) {
         this.firebaseApp = firebaseApp;
      }
      getImagePath(imageKey:string, callback:Function, imageRef?:string) {
@@ -28,6 +29,18 @@ export class ImageManagementService {
              .catch(error=>{console.log("error deleting file")});
          })
          this.af.database.object(imageRef+imageKey).remove();
+     }
+
+     uploadImage(selectedFile, folderName, successCallback) {
+         let generatedKey = this.utilityService.generateRandomString();
+         let storage = this.firebaseApp.storage().ref();
+         let path = `/images/${folderName}/${selectedFile.name}`;
+         let iRef = storage.child(path);
+         iRef.put(selectedFile).then((snapshot)=>{
+             console.log('fileUploadSuccessful');
+             let imageObject:any = {path:path, fileName:selectedFile.name, key:generatedKey, uploadTimeStamp:(new Date()).getTime()};
+             successCallback(imageObject);
+         })
      }
 
 }
