5dfbf417ac1a1ff9b6d82159784a97641f7b6713
jitenagarwal <jitenagarwal19@gmail.com>
2017-04-02 18:41:48 +0530

    multiple image component working

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -8,63 +8,90 @@ import { FormComponentModel } from '../../interfaces/form-component-model.interf
 
 @Component({
     selector: 'image-upload',
-    templateUrl: './image-upload.html'
+    templateUrl: './image-upload.html',
+    styleUrls: ['./image-upload.component.css']
 })
 export class ImageUpload {
     @Input() folder: string;
     @Output() imageUploaded = new EventEmitter();
-    id:string;
-    
+    id: string;
     isUploading: boolean = false;
     firebaseApp: any;
     fileUploadStatus: string = "Please choose a file";
-    private image: any;
+    addedImages: any[] = [];
+    uploadedImages: any[] =[];
 
     constructor(public af: AngularFire, @Inject(FirebaseApp) firebaseApp: any, private utilityService: UtilityService, private imageManagementService: ImageManagementService) {
         this.firebaseApp = firebaseApp;
-        this.id = 'image-upload-input-'+utilityService.generateRandomString(); 
+        this.id = 'image-upload-input-' + utilityService.generateRandomString();
     }
 
     ngOnInit() {
-    
+
     }
     ngOnChanges() {
         this.onComponentUpdate();
+
     }
     onComponentUpdate() {
         let formComponent: FormComponentModel = {
             isValid: this.isComponentValid(),
             invalidMessage: 'Please Upload Image',
-            formModel: this.image,
+            formModel: this.uploadedImages,
             item: 'image-upload'
         };
         this.imageUploaded.emit(formComponent);
     }
     isComponentValid(): boolean {
-        return !this.isUploading && this.image ? true : false;
+        return !this.isUploading && (this.uploadedImages.length === this.addedImages.length) ? true : false;
+    }
+    removeImage(index: number) {
+        this.addedImages.splice(index, 1);
+    }
+
+    private onFileInputChange() {
+        let fileList = this.getFileListFromInput()
+        if (fileList && fileList.length) {
+            for (let i = 0; i < fileList.length; i++) {
+                if (this.utilityService.isFileNameImage(fileList[i].name)) {
+                    this.addedImages.push(fileList[i]);
+                    (function (file) {
+                        file.status = 'Standby';
+                        var reader = new FileReader();
+                        reader.onloadend = function () {
+                            file.src = reader.result;
+                        }
+                        reader.readAsDataURL(file);
+                    })(this.addedImages[this.addedImages.length - 1]);
+                }
+
+            }
+        }
+    }
+    private getFileListFromInput() {
+        return [(<HTMLInputElement>document.getElementById(this.id)).files][0];
     }
 
     uploadImage() {
         var that = this;
-        for (let selectedFile of [(<HTMLInputElement>document.getElementById(this.id)).files[0]]) {
-            if (selectedFile) {
-                if (!this.utilityService.isFileNameImage(selectedFile.name)) {
-                    this.fileUploadStatus = "Select appropriate Format of Image File";
-                    this.isUploading = false;
-                    return;
-                }
-                this.imageManagementService.uploadImage(selectedFile, this.folder, function (imageObj) {
-                    that.af.database.object(`/images/${that.imageManagementService.getFolderName(that.folder)}/${imageObj.key}`).set(imageObj);
-                    that.imageUploadComplete(imageObj);
-                });
-            }
-        }
+        this.addedImages.forEach((imageFile) => {
+            imageFile.status = 'uploading'
+            this.imageManagementService.uploadImage(imageFile, this.folder, function(imageObj) {
+                that.af.database.object(`/images/${that.imageManagementService.getFolderName(that.folder)}/${imageObj.key}`).set(imageObj);
+                imageFile.status = 'upload complete';
+                that.imageUploadComplete(imageObj);
+            });
+        });
     }
     private imageUploadComplete(imageObject: any) {
-        this.isUploading = false;
-        this.image = imageObject;
-        this.fileUploadStatus = "Image Upload successful";
-        this.onComponentUpdate();
+        if (imageObject) {
+            this.uploadedImages.push(imageObject);
+            if (this.uploadedImages.length === this.addedImages.length) {
+                alert('All  images uploaded');
+                this.onComponentUpdate();
+            }
+        }
+        
 
     }
     ngOnDestroy() {
